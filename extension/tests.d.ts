8345a5b2e5e7de36e7665b1708da2006c1ea34f7
mikob <fxrock2002@yahoo.com>
2020-08-01 15:29:37 -0700

    TextInput new tests.

--- extension/tests.d.ts
+++ extension/tests.d.ts
@@ -2,4 +2,5 @@ declare interface ICommandTestContext {
     localPageDomain: string;
     sleep: (amount: number) => Promise<void>;
     activate: () => Promise<void>;
+    waitForTagCondition(client: WebdriverIO.BrowserObject, condition: (numTags) => boolean, errorStr?: string): Promise<number>;
 }
