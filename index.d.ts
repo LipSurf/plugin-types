98b9d219c53abf5805459d4b44a218e73d9855c9
mikob <fxrock2002@yahoo.com>
2020-09-10 21:01:34 -0500

    Cleaned up types so that private types are not included in the public repo lipsurf-types. Instead, they're in lipsurf-common.

--- index.d.ts
+++ index.d.ts
@@ -1,49 +1,4 @@
-declare type plan = 0|10|20;
-
-declare type Term = 'yearly'|'monthly';
-
 // one's we've added ourselves
 declare type GeneralizedLanguageCodes = 'fr'|'de'|'es'|'pt'|'nl';
 // BCP-47
 declare type LanguageCode = GeneralizedLanguageCodes|'af'|'sq'|'am'|'ar'|'ar-DZ'|'ar-BH'|'ar-EG'|'ar-IQ'|'ar-JO'|'ar-KW'|'ar-LB'|'ar-LY'|'ar-MA'|'ar-OM'|'ar-QA'|'ar-SA'|'ar-SY'|'ar-TN'|'ar-AE'|'ar-YE'|'hy'|'as'|'az'|'eu'|'be'|'bn'|'bs'|'bg'|'my'|'ca'|'zh-CN'|'zh-HK'|'zh-MO'|'zh-SG'|'zh-TW'|'hr'|'cs'|'da'|'nl-BE'|'nl-NL'|'en'|'en-AU'|'en-BZ'|'en-CA'|'en-CB'|'en-GB'|'en-IN'|'en-IE'|'en-JM'|'en-NZ'|'en-PH'|'en-ZA'|'en-TT'|'en-US'|'et'|'mk'|'fo'|'fa'|'fi'|'fr-BE'|'fr-CA'|'fr-FR'|'fr-LU'|'fr-CH'|'gd-IE'|'gd'|'de-AT'|'de-DE'|'de-LI'|'de-LU'|'de-CH'|'el'|'gn'|'gu'|'he'|'hi'|'hu'|'is'|'id'|'it-IT'|'it-CH'|'ja'|'kn'|'ks'|'kk'|'km'|'ko'|'lo'|'la'|'lv'|'lt'|'ms-BN'|'ms-MY'|'ml'|'mt'|'mi'|'mr'|'mn'|'ne'|'no-NO'|'or'|'pl'|'pt-BR'|'pt-PT'|'pa'|'rm'|'ro-MO'|'ro'|'ru'|'ru-RU'|'ru-MO'|'sa'|'sr-SP'|'tn'|'sd'|'si'|'sk'|'sl'|'so'|'sb'|'es-AR'|'es-BO'|'es-CL'|'es-CO'|'es-CR'|'es-DO'|'es-EC'|'es-SV'|'es-GT'|'es-HN'|'es-MX'|'es-NI'|'es-PA'|'es-PY'|'es-PE'|'es-PR'|'es-ES'|'es-UY'|'es-VE'|'sw'|'sv-FI'|'sv-SE'|'tg'|'ta'|'tt'|'te'|'th'|'bo'|'ts'|'tr'|'tk'|'uk'|'ur'|'uz-UZ'|'vi'|'cy'|'xh'|'yi'|'zu';
-
-declare interface CardSummarized {
-    lastFour: string;
-    brand: string;
-    exp: string;
-    default: boolean;
-}
-
-declare namespace API {
-    type CreateCheckoutSessionResp = FailResp | CreateCheckoutSessionSuccess;
-
-    interface FailResp {
-        success: false;
-        error: string;
-    }
-
-    type NotFoundResp  = Pick<PlanSettingsSuccess, 'plan'|'costInCents'>;
-
-    interface CreateCheckoutSessionSuccess {
-        success: true;
-        sessionId: string;
-    }
-
-    type PlanSettingsResp = FailResp | NotFoundResp | PlanSettingsSuccess;
-
-    type AddNewCardResp = FailResp | PlanSettingsSuccess;
-
-    interface PlanSettingsSuccess {
-        success: true;
-        status: 'active'|'canceled'|'past_due'|'trialing'|'unpaid'|'incomplete'|'incomplete_expired';
-        // the date in the future when the next payment will take place (null if not an active sub)
-        renewAt: number|null;
-        // the date in the future the sub will be automatically cancelled
-        cancelAt: number|null;
-        endedAt: number|null;
-        term: Term|null;
-        plan: plan;
-        costInCents: number;
-        cards: (CardSummarized|{})[];
-    }
-}
