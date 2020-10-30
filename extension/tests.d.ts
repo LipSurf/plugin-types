29985190a4c5d792982d6434528637ddd2bdcf20
mikob <mikojunky@fastmail.com>
2020-10-30 00:51:15 -0600

    Genesis

---
+++ extension/tests.d.ts
@@ -0,0 +1,6 @@
+declare interface ICommandTestContext {
+    localPageDomain: string;
+    sleep: (amount: number) => Promise<void>;
+    activate: () => Promise<void>;
+    waitForTagCondition(client: WebdriverIO.BrowserObject, condition: (numTags) => boolean, errorStr?: string): Promise<number>;
+}
